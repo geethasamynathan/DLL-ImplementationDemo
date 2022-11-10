d41b8d9d544100ca0bf571de2054a1d0c0cb8f27
geethasamynathan <geethasamynathan2011@gmail.com>
2022-11-10 09:55:25 +0530

    Add project files.

---
+++ CunstomerOrderServiceTestAPP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CustomerOrderLibarary;
+
+namespace CunstomerOrderServiceTestAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  CustomerOrderLibarary.Customer customer = new CustomerOrderLibarary.Customer();
+            Customer customer = new Customer();
+            customer.GetCustomerInfo();
+            customer.DisplayCustomerInfo();
+      
+        }
+    }
+}
---
+++ CustomerOrderLibarary/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerOrderLibarary
+{
+   public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+
+         public void GetCustomerInfo()
+        {
+            Console.WriteLine("enter the CustomerId and Name");
+            CustomerId = Convert.ToInt32(Console.ReadLine());
+            CustomerName = Console.ReadLine();
+        }
+
+
+        public void DisplayCustomerInfo()
+        {
+            Console.WriteLine($" Customer Id : {CustomerId}");
+            Console.WriteLine($" Customer Name :{CustomerName} ");
+        }
+    }
+}
